ecfb841136da5f1f24d39d282e51f96e96a6f7c0
easton <easton.hou@gmail.com>
2018-01-06 07:03:55 +0800

    retry with queue

--- chessmodel/chessdb.py
+++ chessmodel/chessdb.py
@@ -57,7 +57,7 @@ def queryscore_fen(fen):
 
 def queryscore_fen_imply(fen):
     query = urllib.parse.quote(fen)
-    r = requests.get('http://api.chessdb.cn:81/chessdb.php?action=queryscore&board=' + query).text
+    r = requests.get('http://api.chessdb.cn:81/chessdb.php?action=queryscore&board=' + query, timeout=1).text
     if 'unknown' in r:
         return None
     if 'eval' not in r:
@@ -77,24 +77,29 @@ def download_database():
         conn.commit()
     except:
         print('table already created')
-    update_database(conn, fen, set())
+    update_database(conn, [(fen, 15)], set())
 
 
-def update_database(conn, fen, evaluating):
-    if fen in evaluating or is_conclude(conn, fen):
-        return
-    evaluating.add(fen)
-    next_boards, scores = queryboards_fen_imply(fen)
+def update_database(conn, queue, evaluating):
     cursor = conn.cursor()
-    for next_board, score in zip(next_boards, scores):
-        cursor.execute('''INSERT OR IGNORE INTO board_score (board, score) VALUES ('{}', {})'''.format(next_board, score))
-    conn.commit()
-    for next_board in next_boards:
-        update_database(conn, next_board, evaluating)
-    cursor.execute('''UPDATE board_score SET conclude=1 WHERE board='{}' '''.format(fen))
-    conn.commit()
-    evaluating.remove(fen)
-    print('conclude', fen)
+    while len(queue) != 0:
+        fen, score = queue[-1]
+        queue.pop()
+        if score is None:
+            cursor.execute('''UPDATE board_score SET conclude=1 WHERE board='{}' '''.format(fen))
+            conn.commit()
+            evaluating.remove(fen)
+            print('conclude:', fen, 'depth:', len(evaluating), 'queue:', len(queue))
+            continue
+        queue.append((fen, None))
+        evaluating.add(fen)
+        next_boards, scores = queryboards_fen_imply(fen)
+        for next_board, score in zip(next_boards, scores):
+            if next_board in evaluating or is_conclude(conn, next_board):
+                continue
+            cursor.execute('''INSERT OR IGNORE INTO board_score (board, score) VALUES ('{}', {})'''.format(next_board, score))
+            queue.append((next_board, score))
+        conn.commit()
 
 
 def is_conclude(conn, fen):
@@ -109,7 +114,14 @@ def is_conclude(conn, fen):
 
 def queryboards_fen_imply(fen):
     query = urllib.parse.quote(fen)
-    r = requests.get('http://api.chessdb.cn:81/chessdb.php?action=queryall&board=' + query).text.strip('\x00')
+    while True:
+        try:
+            r = requests.get('http://api.chessdb.cn:81/chessdb.php?action=queryall&board=' + query, timeout=1).text.strip('\x00')
+            break
+        except:
+            print('retrying...')
+            continue
+
     if r in ['unknown', 'checkmate']:
         return [], []
     if ':' not in r:
