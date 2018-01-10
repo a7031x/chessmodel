8518af82a33383c8e3af3a40c9fddd2b4df9bfa8
Easton <shho@microsoft.com>
2018-01-10 10:37:25 +0800

    add summary

--- chessmodel/supervised_trainer.py
+++ chessmodel/supervised_trainer.py
@@ -19,12 +19,13 @@ def train(sess, model, batch_board_red_score):
     return loss
 
 
-def run_epoch(taskname, sess, model, dataset, method):
+def run_epoch(taskname, sess, model, dataset, method, sv=None, summary_op=None):
     batch_size = 128
     total_loss = 0
     total = 0
     count = 0
     st = time()
+
     for cursor in range(0, len(dataset), batch_size):
         size = min(len(dataset) - cursor, batch_size)
         batch = dataset[cursor:(cursor+size)]
@@ -34,6 +35,10 @@ def run_epoch(taskname, sess, model, dataset, method):
         total += size
         et = time()
         if et - st >= 20:
+            if sv is not None:
+                feed = create_train_feed(model, batch)
+                summ = sess.run(summary_op, feed)
+                sv.summary_computed(sess, summ)
             print('  ', taskname, '{:.2f}%'.format(total / len(dataset) * 100), 'loss: {:.4f}'.format(np.sqrt(total_loss / total)))
             st = et
     print('finish', taskname, 'loss: {:.4f}'.format(np.sqrt(total_loss / total)))
@@ -62,7 +67,7 @@ def enrich(dataset):
     return r
 
 
-def run_train(sess, model, sv):
+def run_train(sess, model, sv, summary_op):
     iteration = 0
     dataset = list(read_database())
     dataset = [(board, red, score) for board, red, score in dataset if abs(score) <= 5000]
@@ -73,17 +78,20 @@ def run_train(sess, model, sv):
     training_set = enrich(training_set)
     validation_set = dataset[training_size:]
     print('training set:', len(training_set), 'validation set:', len(validation_set))
+    last_error = run_epoch('validing', sess, model, validation_set, evaluate)
     while True:
         random.shuffle(training_set)
-        run_epoch('validing', sess, model, validation_set, evaluate)
-        run_epoch('training', sess, model, training_set, train)
-        sv.saver.save(sess, FLAGS.output_dir, global_step=sv.global_step)
+        run_epoch('training', sess, model, training_set, train, sv, summary_op)
+        error = run_epoch('validing', sess, model, validation_set, evaluate)
+        if error < last_error:
+            sv.saver.save(sess, FLAGS.output_dir, global_step=sv.global_step)
 
 
 if __name__ == '__main__':
     initializer = tf.random_uniform_initializer(-0.05, 0.05)
     with tf.variable_scope('Model', reuse=None, initializer=initializer):
         model = Model()
+    summary_op = tf.summary.merge_all()
     sv = tf.train.Supervisor(logdir=FLAGS.output_dir)
     with sv.managed_session() as sess:
-        run_train(sess, model, sv)
+        run_train(sess, model, sv, summary_op)
